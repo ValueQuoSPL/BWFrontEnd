4e17f73810c871f716cc1e5f02801006fe0505b0
SanjuBoralkar <sanjayboralkar@gmail.com>
2018-11-19 19:24:03 +0530

    bugfix

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -30,10 +30,10 @@ import { NotificationInterceptor } from 'app/blocks/interceptor/notification.int
 import { JhiMainComponent, NavbarComponent, FooterComponent, PageRibbonComponent, ErrorComponent } from 'app/layouts';
 
 // From Downloaded Package
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ChartsModule } from 'ng2-charts';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { DeviceDetectorModule } from 'ngx-device-detector';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
--- src/main/webapp/app/family/familyprofile/familyprofile.component.ts
+++ src/main/webapp/app/family/familyprofile/familyprofile.component.ts
@@ -19,7 +19,7 @@ export class FamilyprofileComponent implements OnInit {
     earncheck = 'notearning';
     account: any;
     date = new FormControl(new Date());
-
+    commonid: any;
     constructor(private Familypro: FamilyprofileService, public commonService: CommonSidebarService) {}
 
     ngOnInit() {
@@ -66,19 +66,23 @@ export class FamilyprofileComponent implements OnInit {
         this.earncheck = 'Earning';
     }
     // editDetail Method to Edit Info of Familyprofile
-    editDetail() {
-        for (let index = 0; index < this.output.length; index++) {
+    editDetail(id) {
+        this.commonid = id;
+        console.log('edit detail id', this.commonid);
+        this.Familypro.updateProfileById(this.commonid).subscribe(res => {
+            this.output = res;
             this.isValid = false;
-            this.familyProfile.relationship = this.output[index].relationship;
-            this.familyProfile.firstname = this.output[index].firstname;
-            this.familyProfile.middlename = this.output[index].middlename;
-            this.familyProfile.lastname = this.output[index].lastname;
-            this.familyProfile.dateOfBirth = this.output[index].dateOfBirth;
-            this.familyProfile.email = this.output[index].email;
-            this.familyProfile.phonenumber = this.output[index].phonenumber;
+            console.log('response of update profile by id id', this.output);
+            this.familyProfile.relationship = this.output.relationship;
+            this.familyProfile.firstname = this.output.firstname;
+            this.familyProfile.middlename = this.output.middlename;
+            this.familyProfile.lastname = this.output.lastname;
+            this.familyProfile.dateOfBirth = this.output.dateOfBirth;
+            this.familyProfile.email = this.output.email;
+            this.familyProfile.phonenumber = this.output.phonenumber;
             this.familyProfile.uid = this.uid;
             this.show = false;
-        }
+        });
         // this.isValid = false;
         // this.familyProfile.relationship = this.output[0].relationship;
         // this.familyProfile.firstname = this.output[0].firstname;
@@ -93,6 +97,7 @@ export class FamilyprofileComponent implements OnInit {
     // update Method to Update Info of Familyprofile
 
     update() {
+        this.familyProfile.id = this.commonid;
         this.Familypro.updateProfile(this.familyProfile).subscribe(responce => {
             this.getFamilyProfilebyid();
         });
