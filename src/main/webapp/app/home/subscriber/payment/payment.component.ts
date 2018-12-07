560f8ca1be9fd893b6c6c5621dd91fa85cc4b815
pratik-bali <pratik.bali@valuequo.com>
2018-12-07 16:36:07 +0530

    Update payment.component.ts

--- src/main/webapp/app/home/subscriber/payment/payment.component.ts
+++ src/main/webapp/app/home/subscriber/payment/payment.component.ts
@@ -15,9 +15,7 @@ export class PaymentComponent implements OnInit {
     disablePaymentButton1 = false;
     disablePaymentButton2 = true;
     sUrl = 'https://www.buckswise.com:8443/api/success';
-    //sUrl = 'http://www.buckswise.com:8080/api/success';
     fUrl = 'https://www.buckswise.com:8443/api/success';
-    //fUrl = 'http://www.buckswise.com:8080/api/success';
     public paymentDetail: any = [];
 
     @Input() offer;
