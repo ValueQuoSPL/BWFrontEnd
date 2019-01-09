1297a0fa12f7f7479dbceb425524a62acaf53b44
SanjuBoralkar <sanjayboralkar@gmail.com>
2019-01-09 19:17:07 +0530

    how it work,mutul-fund,userManagement

--- src/main/webapp/app/my-assets/mutual/mutual.component.ts
+++ src/main/webapp/app/my-assets/mutual/mutual.component.ts
@@ -14,6 +14,7 @@ import { MatDialog } from '@angular/material';
 })
 export class MutualComponent implements OnInit {
     user: any;
+    type: any;
     uid: any;
     output: any;
     getdata: any;
