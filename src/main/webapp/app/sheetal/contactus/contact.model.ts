f2095e02d3776f82ea3a567e04b754281a1a43f0
sandeeppote <38154598+sandeeppote@users.noreply.github.com>
2019-01-16 13:28:04 +0530

    refer, contact update

--- src/main/webapp/app/sheetal/contactus/contact.model.ts
+++ src/main/webapp/app/sheetal/contactus/contact.model.ts
@@ -1,6 +1,7 @@
 export class UserContact {
-  name: string;
-  email: string;
-  phone: string;
-  msg: string;
+    name: string;
+    email: string;
+    phone: string;
+    msg: string;
+    yourname: string;
 }
