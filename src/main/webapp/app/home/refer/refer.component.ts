a5d0d1475dd1c11679a4dcc4f74274766bd5924b
pratik-bali <pratik.bali@valuequo.com>
2019-02-12 19:40:52 +0530

    Refer friend code changes

--- src/main/webapp/app/home/refer/refer.component.ts
+++ src/main/webapp/app/home/refer/refer.component.ts
@@ -1,6 +1,7 @@
 import { UserContact } from 'app/sheetal/contactus/contact.model';
 import { ContactService } from 'app/sheetal/contactus/contact.service';
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
     selector: 'jhi-refer',
@@ -8,16 +9,30 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./refer.component.css', '../../css/universal.css']
 })
 export class ReferComponent implements OnInit {
-    isDisable: Boolean = false;
     user: UserContact = new UserContact();
     constructor(private contactService: ContactService) {}
     // isDataSaved: Boolean = false;
+    isDisabled: Boolean = false; // added by ranjan
     submitUser() {
         this.contactService.refferSave(this.user).subscribe();
-        this.isDisable = !this.isDisable;
         // this.isDataSaved = true;
         // alert('Thank you for your Intrest we will shortly contact you');
+        this.isDisabled = !this.isDisabled; // added by ranjan
+        alert('Thank you for reffering. Your message has been emailed to your friend'); // added by ranjan
     }
 
-    ngOnInit() {}
+    // added by ranjan start
+    ngOnInit() {
+        // Validation for name
+        $('#yourname').bind('keypress', function(event) {
+            const charCode = event.which;
+            if (charCode === 8 || charCode === 0) {
+                return;
+            } else {
+                const keyChar = String.fromCharCode(charCode);
+                return /[a-zA-Z\s]/.test(keyChar);
+            }
+        });
+    }
+    // added by ranjan  ends
 }
