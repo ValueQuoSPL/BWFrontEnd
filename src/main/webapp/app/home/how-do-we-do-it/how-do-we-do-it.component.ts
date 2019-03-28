a4bf6df94078c30743f1f54cd7cbad36ec820e7e
sheetalsrivas <36916358+sheetalsrivas@users.noreply.github.com>
2019-03-28 18:55:12 +0530

    how it works

--- src/main/webapp/app/home/how-do-we-do-it/how-do-we-do-it.component.ts
+++ src/main/webapp/app/home/how-do-we-do-it/how-do-we-do-it.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'jhi-how-do-we-do-it',
     templateUrl: './how-do-we-do-it.component.html',
-    styleUrls: ['../../css/universal.css'],
+    styleUrls: ['../../css/universal.css', './HowDoBackground.css'],
     providers: [NgbCarouselConfig]
 })
-export class HowDoWeDoItComponent implements OnInit {
-    images = [1, 2, 3].map(() => `${Math.random()}`);
+export class HowDoWeDoItComponent {
+    showNavigationArrows = false;
+    showNavigationIndicators = false;
+    images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
 
-    ngOnInit() {}
+    constructor(config: NgbCarouselConfig, private _http: HttpClient) {
+        // customize default values of carousels used by this component tree
+        config.showNavigationArrows = true;
+        config.showNavigationIndicators = true;
+    }
 }
